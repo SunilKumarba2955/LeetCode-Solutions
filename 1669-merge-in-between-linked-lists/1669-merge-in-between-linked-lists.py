e53ec04342bbf6f4496dedb03e7f25053d68f19f
Sunil Kumar B A <90314405+SunilKumarba2955@users.noreply.github.com>
2024-03-21 00:43:24 +0530

    Time: 172 ms (98.56%), Space: 21.1 MB (70.92%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
@@ -5,17 +5,16 @@
 #         self.next = next
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-        ptr = list1
-        for _ in range(a - 1):
-            ptr = ptr.next
-        
-        qtr = ptr.next
-        for _ in range(b - a + 1):
-            qtr = qtr.next
-        
-        ptr.next = list2
-        while list2.next:
-            list2 = list2.next
-        list2.next = qtr
-        
+        ptr3 = list2
+        while ptr3.next:
+            ptr3 = ptr3.next
+        ptr1 = list1
+        for i in range(a-1):
+            ptr1 = ptr1.next
+        ptr2 = ptr1
+        for i in range(a, b+2):
+            ptr2 = ptr2.next
+        # ptr2 = ptr2.next
+        ptr1.next = list2
+        ptr3.next = ptr2
         return list1
