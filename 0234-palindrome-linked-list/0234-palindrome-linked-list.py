63dfb5866260aa57d53ee2a098ad7129abe45ab7
Sunil Kumar B A <90314405+SunilKumarba2955@users.noreply.github.com>
2025-02-09 16:47:38 +0530

    Time: 39 ms (41.65%), Space: 34.8 MB (80.32%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,25 +4,32 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+
+    def reverse(self, tempHead: Optional[ListNode]) -> Optional[ListNode]:
+        nxt = tempHead.next if tempHead else tempHead
+        cur = tempHead
+        if cur:
+            cur.next = None
+        while nxt:
+            temp = nxt.next
+            nxt.next = cur
+            cur = nxt
+            nxt = temp
+
+        return cur
+            
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        slow, fast = head, head.next
-        while fast!=None and fast.next!=None:
+        
+        slow, fast, prev = head, head, None
+        while fast and fast.next:
+            prev = slow
             slow = slow.next
             fast = fast.next.next
-        
-        prev = None
-        curr = slow.next
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        slow.next = None
-        f = head
-        s = prev
-        while s!=None:
-            if s.val!=f.val:
-                return False
-            s = s.next
-            f = f.next
-        return True
+        if prev:
+            prev.next = None
+        temp = slow.next if fast else slow
+        head1, head2 = head, self.reverse(temp)
+        while head1 and head2 and head1.val == head2.val:
+            head1 = head1.next
+            head2 = head2.next
+        return False if head1 and head2 else True
